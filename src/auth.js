efebce2ebee2834e269f0f0c393aa8a42e5bafed
arlind <squidget788@gmail.com>
2026-03-06 10:30:06 -0500

    Separate auth.js from 1 File

--- src/auth.js
+++ src/auth.js
@@ -1,1 +1,16 @@
+export function parseCookies(cookieHeader) {
+  const cookies = {};
+  if (!cookieHeader) return cookies;
 
+  cookieHeader.split(";").forEach(part => {
+    const [key, ...rest] = part.trim().split("=");
+    cookies[key] = rest.join("=");
+  });
+
+  return cookies;
+}
+
+export function isAdminAuthenticated(request, env) {
+  const cookies = parseCookies(request.headers.get("Cookie"));
+  return cookies.admin_session === env.ADMIN_SESSION_TOKEN;
+}
