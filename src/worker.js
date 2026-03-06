3a961c0e9cac3315a2a1ba24bbd60797e30c1bb3
arlind <squidget788@gmail.com>
2026-03-06 10:29:27 -0500

    Separate worker.js from 1 File

--- src/worker.js
+++ src/worker.js
@@ -1,1 +1,37 @@
+import { getLeaderboard } from "./leaderboard.js";
+import { isAdminAuthenticated } from "./auth.js";
 
+export default {
+  async fetch(request, env) {
+
+    const url = new URL(request.url);
+    const path = url.pathname;
+
+    let pageContent = "";
+
+    if (path === "/") {
+      pageContent = "<h1>we are the future of music.</h1>";
+    }
+
+    else if (path === "/current") {
+      const data = await getLeaderboard("Sheet1");
+      // build leaderboard HTML
+    }
+
+    else if (path === "/admin") {
+      const loggedIn = isAdminAuthenticated(request, env);
+      // admin logic
+    }
+
+    const html = `
+      <html>
+      ...
+      ${pageContent}
+      </html>
+    `;
+
+    return new Response(html, {
+      headers: { "content-type": "text/html;charset=UTF-8" }
+    });
+  }
+};
